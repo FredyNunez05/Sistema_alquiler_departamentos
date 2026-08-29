9e249f5b16ea9f9248075c6b941278efb1cf6e85
FredyNunez05 <ncfrdy005@gmail.com>
2026-08-29 14:41:18 -0500

    Inicializar proyecto

---
+++ src/sistema_alquiler_departamentos/Sistema_alquiler_departamentos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistema_alquiler_departamentos;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Sistema_alquiler_departamentos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
